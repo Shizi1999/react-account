cf1f81d6035af9acfb3fd1945b36b65aa65b23dd
Shizi1999 <daungocduc1999@gmail.com>
2023-02-17 12:00:04 +0700

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './index.css';
 
 const queryClient = new QueryClient();
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,51 @@
+import { useState } from 'react';
+import axiosClient from '../../api/axiosClient';
+
 const Home: React.FC = () => {
+  // const [files, setFiles] = useState<Blob[]>([]);
+  // const hanldeChangeFile = ({ currentTarget: { files } }: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (files && files.length) {
+  //     setFiles((existing) => existing.concat(Array.from(files)));
+  //   }
+  // };
+
+  // const uploadImage = () => {
+  //   const formData = new FormData();
+  //   console.log(files);
+  //   files.forEach((file) => formData.append('image', file));
+  //   axiosClient
+  //     .post('/test', formData, {
+  //       headers: {
+  //         'content-type': 'multipart/form-data',
+  //       },
+  //     })
+  //     .then((res) => console.log(res));
+  // };
+
+  // const removeImage = (index: number) => {
+  //   setFiles((prev) => prev.filter((item, i) => index !== i));
+  // };
+
   return (
     <div className="flex items-center justify-center h-full text-blue-700 font-medium text-2xl">
       Start With React Login OAuth2
+      {/* <input type="file" multiple onChange={hanldeChangeFile} />
+      <div className="">
+        {files.length > 0 &&
+          files.map((file, index) => {
+            return (
+              <div
+                onClick={() => {
+                  removeImage(index);
+                }}
+                key={index}
+              >
+                {URL.createObjectURL(file)}
+              </div>
+            );
+          })}
+      </div> */}
+      {/* <button onClick={uploadImage}>Upload</button> */}
     </div>
   );
 };
